a5b73e0ccaaa2b7b4922278bfca617298b157120
Roger LinkRiver <83733039+roger-linkriver@users.noreply.github.com>
2022-11-22 11:38:09 +0100

    Add files via upload

---
+++ polygon-mumbai/HTTP GET > Int256 Without Multiply/APIConsumer.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+
+/**
+ * Request testnet LINK and ETH here: https://faucets.chain.link/
+ * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/
+ */
+
+/**
+ * THIS IS AN EXAMPLE CONTRACT WHICH USES HARDCODED VALUES FOR CLARITY.
+ * PLEASE DO NOT USE THIS CODE IN PRODUCTION.
+ */
+contract APIConsumer is ChainlinkClient, ConfirmedOwner {
+    using Chainlink for Chainlink.Request;
+
+    int256 public value;
+    bytes32 private jobId;
+    uint256 private fee;
+
+    event RequestVolume(bytes32 indexed requestId, int256 value);
+
+    /**
+     * @notice Initialize the link token and target oracle
+     *
+     * Polygon Mumbai Testnet details:
+     * Link Token: 0x326C977E6efc84E512bB9C30f76E30c160eD06FB
+     * Oracle: 0xd5a5FE4e1a8a41246cE8fBC876270f87aeE281C5 (LinkRiver)
+     * jobId: 91e37d111e344c3b838a06ea7a2f07a7
+     *
+     */
+    constructor() ConfirmedOwner(msg.sender) {
+        setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
+        setChainlinkOracle(0xd5a5FE4e1a8a41246cE8fBC876270f87aeE281C5);
+        jobId = "91e37d111e344c3b838a06ea7a2f07a7";
+        fee = 0; 
+    }
+
+    /**
+     * Create a Chainlink request to retrieve API response and find the target
+     * data.
+     * Replace the example parameters with ones appropriate to your needs.
+     */
+    function requestValue() public returns (bytes32 requestId) {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfill.selector
+        );
+
+        // Set the URL to perform the GET request on
+        req.add(
+            "get",
+            "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD"
+        );
+
+        // Set the path to find the desired data in the API response, where the response format is:
+        // {"RAW":
+        //   {"ETH":
+        //    {"USD":
+        //     {
+        //      "CHANGEDAY": +/-xxx.xxx,
+        //     }
+        //    }
+        //   }
+        //  }
+
+        req.add("path", "RAW,ETH,USD,CHANGEDAY");
+
+        // Sends the request
+        return sendChainlinkRequest(req, fee);
+    }
+
+    /**
+     * Receive the response in the form of int256
+     */
+    function fulfill(
+        bytes32 _requestId,
+        int256 _value
+    ) public recordChainlinkFulfillment(_requestId) {
+        emit RequestVolume(_requestId, _value);
+        value = _value;
+    }
+
+    /**
+     * Allow withdraw of Link tokens from the contract
+     */
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+        require(
+            link.transfer(msg.sender, link.balanceOf(address(this))),
+            "Unable to transfer"
+        );
+    }
+}
